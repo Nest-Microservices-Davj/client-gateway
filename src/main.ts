5c0ae583c1985fc838d4cb3d6fc3c260158c03ad
Daniel Vidal <daniel.vidal@globant.com>
2024-05-21 21:43:42 -0500

    Test message

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,8 @@ async function bootstrap() {
 
   app.useGlobalFilters(new RpcCustomExceptionFilter());
 
+  console.log('Tesst');
+
   await app.listen(envs.port);
 
   logger.log(`Gateway running on port ${envs.port}`);
