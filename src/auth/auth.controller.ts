ac0d8ad16894d5fac2fb25fa8cb4f96c2f56be86
Daniel Vidal <vidal179@gmail.com>
2024-06-04 07:07:03 -0500

    The auth servive has been imlemented

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post, UseGuards } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginUserDto } from './dto';
 import { catchError } from 'rxjs';
+import { AuthGuard } from './guards/auth.guard';
+import { User, Token } from './decorator';
+import { CurrentUser } from './interfaces/current-user.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -22,11 +25,17 @@ export class AuthController {
 
   @Post('login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
-    return this.clientProxy.send('auth.login.user', loginUserDto);
+    return this.clientProxy.send('auth.login.user', loginUserDto).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
   }
 
+  @UseGuards(AuthGuard)
   @Get('verify')
-  verifyUser() {
-    return this.clientProxy.send('auth.verify.user', {});
+  verifyUser(@User() user: CurrentUser, @Token() token: string) {
+    return { user, token };
+    //return this.clientProxy.send('auth.verify.user', {});
   }
 }
